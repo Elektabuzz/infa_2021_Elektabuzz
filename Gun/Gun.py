24eef66c8e5cfb93a001592e2bd0fbc30d22bf97
Zabilskyi Daniil <zabilskii.d@phystech.edu>
2021-11-21 22:58:54 +0300

    Сделал танк

--- Gun/Gun.py
+++ Gun/Gun.py
@@ -17,10 +17,11 @@ GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 width_of_window = 800
 height_of_window = 600
+v_max: int = 35
 
 
 class Ball:
-    def __init__(self, x=40, y=450, live=3, k=0.9):
+    def __init__(self, gun, live=3, k=0.9):
 
         """ Конструктор класса ball
         Args:
@@ -31,8 +32,8 @@ class Ball:
         """
         self.k = k
         self.ball_screen = screen
-        self.x = x
-        self.y = y
+        self.x = gun.x1
+        self.y = gun.y1
         self.r = 10
         self.vx = 0
         self.vy = 0
@@ -124,11 +125,16 @@ class Gun:
         self.f2_on = 0
         self.an = 1
         self.color = GREY
+        self.color2 = GREEN
         self.x1 = x1
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
         self.width = width
+        self.d = 80
+        self.h = 80
+        self.vx = 5
+        self.vy = 5
 
     def fire2_start(self):
         """Запускание подготовки к выстрелу"""
@@ -142,7 +148,7 @@ class Gun:
             end_event - событие отпускания мыши
             *array_balls - массив мячей
         """
-        new_ball = FirstTypeBullet()
+        new_ball = FirstTypeBullet(gun)
         self.an = math.atan2((end_event.pos[1] - new_ball.y), (end_event.pos[0] - new_ball.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
@@ -158,7 +164,7 @@ class Gun:
             end_event - событие отпускания мыши
             *array_balls - массив мячей
         """
-        new_ball = SecondTypeBullet()
+        new_ball = SecondTypeBullet(gun)
         self.an = math.atan2((end_event.pos[1] - new_ball.y), (end_event.pos[0] - new_ball.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
@@ -172,9 +178,16 @@ class Gun:
             targetting_event - событие изменение позиции мыши
         """
         if targetting_event:
-            self.an = math.atan((targetting_event.pos[1] - 450) / (targetting_event.pos[0] - 20))
+            self.an = math.atan((targetting_event.pos[1] - self.y1) / (targetting_event.pos[0] - (self.x1 - 20)))
             self.x2 = self.x1 + self.f2_power * math.cos(self.an)
             self.y2 = self.y1 + self.f2_power * math.sin(self.an)
+            if targetting_event.pos[0] - (self.x1 - 20) > 0:
+                self.x2 = self.x2
+                self.y2 = self.y2
+            else:
+                self.x2 = 2*self.x1 - self.x2
+                self.y2 = 2*self.y1 - self.y2
+
         if self.f2_on:
             self.color = RED
         else:
@@ -182,6 +195,7 @@ class Gun:
 
     def draw(self):
         """Функция рисует пушку"""
+        pygame.draw.rect(self.gun_screen, self.color2, (self.x1 - 40, self.y1 - 40, self.d, self.h))
         pygame.draw.line(self.gun_screen, self.color, (self.x1, self.y1), (self.x2, self.y2), self.width)
 
     def power_up(self):
@@ -193,23 +207,58 @@ class Gun:
         else:
             self.color = GREY
 
-    def hit_first_type_bullet(self, points=1):
+    def hit_first_type_bullet(self, obj, points=1):
         """Попадание шарика в цель.
         Args:
             points - шаг изменения количества очков при попадании в цель
+            obj - объект
         """
-        self.scores += points
+        if type(obj) == FirstTypeTarget:
+            self.scores += points
+        elif type(obj) == SecondTypeTarget:
+            self.scores += 2 * points
+        else:
+            print('Вы попали не в мишень')
         print('scores:', self.scores)
         return self.scores
 
-    def hit_second_type_bullet(self, points=3):
+    def hit_second_type_bullet(self, obj, points=3):
         """Попадание шарика в цель.
         Args:
             points - шаг изменения количества очков при попадании в цель
         """
-        self.scores += points
+        if type(obj) == FirstTypeTarget:
+            self.scores += points
+        elif type(obj) == SecondTypeTarget:
+            self.scores += 2 * points
+        else:
+            print('Вы попали не в мишень')
         print('scores:', self.scores)
         return self.scores
+
+    def move_d(self):
+        """
+        Двигает танк вправо
+        """
+        self.x1 += self.vx
+
+    def move_a(self):
+        """
+        Двигает танк влево
+        """
+        self.x1 -= self.vx
+
+    def move_w(self):
+        """
+        Двигает танк вверх
+        """
+        self.y1 -= self.vy
+
+    def move_s(self):
+        """
+        Двигает танк вниз
+        """
+        self.y1 += self.vy
 
 
 class Target:
@@ -233,9 +282,10 @@ class Target:
         """Функция рисует объект"""
         pygame.draw.circle(self.target_screen, self.color, (self.x, self.y), self.r)
 
+
+class FirstTypeTarget(Target):
     def move(self):
         """Перемещение цели по прошествии единицы времени"""
-
         self.x += self.vx
         self.y -= self.vy
         if (self.x - self.r) < 0:
@@ -252,6 +302,38 @@ class Target:
             self.y = height_of_window - self.r
 
 
+class SecondTypeTarget(Target):
+    def move(self):
+        """Перемещение цели по прошествии единицы времени"""
+
+        self.x += self.vx
+        self.y -= self.vy
+        if (self.x - self.r) < 0:
+            self.x = self.r
+            if abs(self.vx) <= v_max:
+                self.vx = -2 * self.vx
+            else:
+                self.vx = -self.vx
+        elif (self.x + self.r) > width_of_window:
+            self.x = width_of_window - self.r
+            if abs(self.vx) <= v_max:
+                self.vx = -2 * self.vx
+            else:
+                self.vx = -self.vx
+        if (self.y - self.r) < 0:
+            self.y = self.r
+            if abs(self.vy) <= v_max:
+                self.vy = -2 * self.vy
+            else:
+                self.vy = -self.vy
+        elif (self.y + self.r) > height_of_window:
+            if abs(self.vy) <= v_max:
+                self.vy = -2 * self.vy
+            else:
+                self.vy = -self.vy
+            self.y = height_of_window - self.r
+
+
 pygame.init()
 screen = pygame.display.set_mode((width_of_window, height_of_window))
 
@@ -260,9 +342,10 @@ second_type_bullets = []
 
 clock = pygame.time.Clock()
 gun = Gun()
-target1 = Target()
-target2 = Target()
+target1 = FirstTypeTarget()
+target2 = SecondTypeTarget()
 finished = False
+flD = flA = flW = flS = False
 
 while not finished:
     screen.fill(WHITE)
@@ -288,6 +371,26 @@ while not finished:
                 gun.fire2_right_end(event, second_type_bullets)
         elif event.type == pygame.MOUSEMOTION:
             gun.targetting(event)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                flD = True
+            elif event.key == pygame.K_a:
+                flA = True
+            elif event.key == pygame.K_w:
+                flW = True
+            elif event.key == pygame.K_s:
+                flS = True
+        elif event.type == pygame.KEYUP:
+            flD = flA = flW = flS = False
+        elif flD:
+            gun.move_d()
+        elif flA:
+            gun.move_a()
+        elif flW:
+            gun.move_w()
+        elif flS:
+            gun.move_s()
+
     target1.move()
     target2.move()
 
@@ -295,13 +398,13 @@ while not finished:
         i.move()
         if i.hittest(target1):
             target1.live = 0
-            gun.hit_first_type_bullet()
-            target1 = Target()
+            gun.hit_first_type_bullet(target1)
+            target1 = FirstTypeTarget()
         else:
             if i.hittest(target2):
                 target2.live = 0
-                gun.hit_first_type_bullet()
-                target2 = Target()
+                gun.hit_first_type_bullet(target2)
+                target2 = SecondTypeTarget()
         if i.live <= 0:
             first_type_bullets.pop(first_type_bullets.index(i))
 
@@ -309,14 +412,14 @@ while not finished:
         i.move()
         if i.hittest(target1):
             target1.live = 0
-            gun.hit_second_type_bullet()
-            target1 = Target()
+            gun.hit_second_type_bullet(target1)
+            target1 = FirstTypeTarget()
             second_type_bullets.pop(second_type_bullets.index(i))
         else:
             if i.hittest(target2):
                 target2.live = 0
-                gun.hit_second_type_bullet()
-                target2 = Target()
+                gun.hit_second_type_bullet(target2)
+                target2 = SecondTypeTarget()
                 second_type_bullets.pop(second_type_bullets.index(i))
         if i.live <= 0:
             second_type_bullets.pop(second_type_bullets.index(i))
